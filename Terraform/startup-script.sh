dc2e822ceae3c4b5f47f12d4bcb7ea5d5873cbe4
David <david.illner.x@gmail.com>
2025-04-09 14:53:56 +0200

    Minor changes and tweaks

--- Terraform/startup-script.sh
+++ Terraform/startup-script.sh
@@ -2,7 +2,6 @@
 set -e
 
 # Ensure the package list is up-to-date and perform an upgrade
-<<<<<<< HEAD
 apt-get update -y
 apt-get upgrade -y
 
@@ -29,31 +28,3 @@ rm -f Anaconda3-2024.10-1-Linux-x86_64.sh
 # Add Anaconda to PATH (system-wide)
 echo 'export PATH="/opt/anaconda3/bin:$PATH"' | sudo tee /etc/profile.d/anaconda3.sh
 chmod +x /etc/profile.d/anaconda3.sh
-=======
-sudo apt-get update -y
-sudo apt-get upgrade -y
-
-# Install dependencies with sudo
-sudo apt-get install -y wget curl lsb-release sudo
-
-# Install Docker
-sudo apt-get install -y docker.io
-
-# Install Docker Compose
-sudo mkdir -p bin
-cd bin
-sudo wget https://github.com/docker/compose/releases/download/v2.34.0/docker-compose-linux-x86_64 -O docker-compose
-sudo chmod +x docker-compose
-echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
-cd ..
-
-# Install Anaconda
-sudo wget https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
-sudo bash ~/Anaconda3-2024.10-1-Linux-x86_64.sh -b -p $HOME/anaconda3
-source ~/anaconda3/bin/activate
-conda init --all
-rm -f Anaconda3-2024.10-1-Linux-x86_64.sh 
-
-# Ensure the changes take effect
-source ~/.bashrc
->>>>>>> 2ad4264 (Initial commit with .gitignore)
